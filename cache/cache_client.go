bc8f08bea9e0c71dd6de45f3f37e67c9e4ba375b
wumengye <wumengye@cartx.ai>
2022-08-08 17:30:03 +0800

    第一次提交

---
+++ cache/cache_client.go
@@ -0,0 +1,97 @@
+package cache
+
+import (
+	"context"
+	"errors"
+	"github.com/bsm/redislock"
+	"github.com/go-redis/redis/v8"
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/os/gctx"
+	"time"
+)
+
+type Storage interface {
+	Initialized() bool
+	Get(ctx context.Context, key string) ([]byte, error)
+	GetString(ctx context.Context, key string) (string, error)
+	Set(ctx context.Context, key string, content []byte, duration time.Duration) error
+	SetString(ctx context.Context, key string, content string, duration time.Duration) error
+	Delete(ctx context.Context, keys []string) error
+	SAdd(ctx context.Context, setKey, value string) error
+	SMembers(ctx context.Context, setKey string) ([]string, error)
+}
+
+var (
+	RedisClient            *redis.Client
+	RedisLocker            *redislock.Client
+	LockTimeout            time.Duration
+	storage                Storage
+	ErrNilResult           = redis.Nil
+	ErrCacheNotInitialized = errors.New("cache: not initialized")
+	ErrEmptyCacheKey       = errors.New("cache: cache key is empty")
+	ErrLockTimeout         = errors.New("cache: lock timeout")
+	ErrNotFound            = errors.New("cache: not found")
+)
+
+func init() {
+	ctx := gctx.New()
+	lockTimeoutSeconds := g.Cfg().MustGet(ctx, "redis.default.lockTimeoutSeconds", 3).Int()
+	LockTimeout = time.Duration(lockTimeoutSeconds) * time.Second
+	RedisClient = redis.NewClient(&redis.Options{
+		Network:  "tcp",
+		Addr:     g.Cfg().MustGet(ctx, "redis.default.host").String(),
+		DB:       g.Cfg().MustGet(ctx, "redis.default.db").Int(),
+		Password: g.Cfg().MustGet(ctx, "redis.default.password").String(),
+	})
+	info := RedisClient.Info(ctx)
+	cacheInitialized := false
+	if info.Err() != nil {
+		g.Log().Warningf(ctx, "Error initialize redis: %s", info.Err().Error())
+	} else {
+		cacheInitialized = true
+	}
+	if RedisClient != nil {
+		RedisLocker = redislock.New(RedisClient)
+	}
+	storage = &redisStorage{
+		initialized: cacheInitialized,
+		Client:      RedisClient,
+	}
+}
+
+type redisStorage struct {
+	initialized bool
+	Client      *redis.Client
+}
+
+func (s *redisStorage) Initialized() bool {
+	return s.initialized
+}
+
+func (s *redisStorage) Get(ctx context.Context, key string) ([]byte, error) {
+	return s.Client.Get(ctx, key).Bytes()
+}
+
+func (s *redisStorage) GetString(ctx context.Context, key string) (string, error) {
+	return s.Client.Get(ctx, key).Result()
+}
+
+func (s *redisStorage) Set(ctx context.Context, key string, content []byte, duration time.Duration) error {
+	return s.Client.Set(ctx, key, content, duration).Err()
+}
+
+func (s *redisStorage) SetString(ctx context.Context, key string, content string, duration time.Duration) error {
+	return s.Client.Set(ctx, key, content, duration).Err()
+}
+
+func (s *redisStorage) Delete(ctx context.Context, keys []string) error {
+	return s.Client.Del(ctx, keys...).Err()
+}
+
+func (s *redisStorage) SAdd(ctx context.Context, setKey, value string) error {
+	return s.Client.SAdd(ctx, setKey, value).Err()
+}
+
+func (s *redisStorage) SMembers(ctx context.Context, setKey string) ([]string, error) {
+	return s.Client.SMembers(ctx, setKey).Result()
+}
---
+++ cache/cache_util.go
@@ -0,0 +1,152 @@
+package cache
+
+import (
+	"context"
+	"github.com/bsm/redislock"
+	"github.com/gogf/gf/v2/encoding/gjson"
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/os/gtime"
+	"github.com/gogf/gf/v2/text/gstr"
+	"time"
+)
+
+const ServiceCachePrefix = "_SC_"
+const ServiceCacheKeySetPrefix = "_SC_SET_"
+const ServiceCacheLockerPrefix = "_LOCK"
+
+func Initialized() bool {
+	return storage.Initialized()
+}
+
+// GetCacheKey 生成cacheKey
+// serviceName service名称，不同service名称不要相同，否则会造成 cacheKey 冲突，可以将 serviceName 当做某些缓存实现的 namespace 看待
+// funcName method名称，不同funcName名称不要相同，否则会造成 cacheKey 冲突
+// funcParams 所有的method参数
+func GetCacheKey(serviceName string, funcName string, funcParams ...any) *string {
+	var keyModelString []string
+	if funcParams != nil {
+		for _, one := range funcParams {
+			if one == nil {
+				continue
+			}
+			keyModelString = append(keyModelString, gjson.MustEncodeString(one))
+		}
+	}
+	cacheKey := ServiceCachePrefix + serviceName + "_" + funcName + "(" + gstr.Join(keyModelString, "_") + ")"
+	return &cacheKey
+}
+
+// RetrieveCacheTo 根据 cacheKey 获取缓存对象，并通过 json 解码到 value 中
+// value 应该是原始对象的指针，必须在外部先初始化该对象
+func RetrieveCacheTo(ctx context.Context, cacheKey *string, value any) error {
+	if cacheKey == nil {
+		return ErrEmptyCacheKey
+	}
+	if !storage.Initialized() {
+		return ErrCacheNotInitialized
+	}
+	var (
+		lock         *redislock.Lock
+		lockObtained = false
+		err          error
+	)
+	for !lockObtained {
+		startTimestamp := gtime.Now()
+		// 对每次取 cache  给最长 3秒钟处理时间，在此期间到达的同样取 cache 请求会等待当前处理结束
+		lock, err = RedisLocker.Obtain(ctx, ServiceCacheLockerPrefix+*cacheKey, LockTimeout, nil)
+		if err == redislock.ErrNotObtained { // 反复等待，直到上一个同 cacheKey 的取 cache 操作结束
+			time.Sleep(10 * time.Millisecond)
+			if gtime.Now().After(startTimestamp.Add(LockTimeout)) { // 等待时间超过 3 秒钟，返回 nil
+				g.Log().Errorf(ctx, "Timeout obtaining lock for cache \"%s\"", *cacheKey)
+				return ErrLockTimeout
+			}
+			continue
+		} else if err != nil {
+			g.Log().Errorf(ctx, "Error obtaining lock for cache \"%s\"", *cacheKey)
+			return err
+		}
+		lockObtained = true
+	}
+	defer func(lock *redislock.Lock, ctx context.Context) {
+		_ = lock.Release(ctx)
+	}(lock, ctx)
+
+	cachedValue, err := storage.Get(ctx, *cacheKey)
+	if err == ErrNilResult {
+		return ErrNotFound
+	} else if err != nil {
+		return err
+	}
+	err = gjson.DecodeTo(cachedValue, value)
+	if err != nil {
+		g.Log().Warningf(ctx, "error decoding cache value \"%s\" for key \"%s\"", cachedValue, *cacheKey)
+		return err
+	}
+	return nil
+}
+
+// SaveCache 添加缓存
+// serviceName service名称，需要的原因是要记录当前 service 下所有已经保存的缓存 key 的集合
+// cacheKey 缓存key
+// value 要放入缓存的value，保存前会对其进行 json 编码
+func SaveCache(ctx context.Context, serviceName string, cacheKey *string, value any) error {
+	if cacheKey == nil {
+		return ErrEmptyCacheKey
+	}
+	if !storage.Initialized() {
+		return ErrCacheNotInitialized
+	}
+	valueBytes, err := gjson.Encode(value)
+	if err != nil {
+		g.Log().Warningf(ctx, "error encoding cache value for key \"%s\"", *cacheKey)
+		return err
+	}
+	err = storage.Set(ctx, *cacheKey, valueBytes, 10*time.Minute)
+	if err != nil {
+		g.Log().Warningf(ctx, "error save cache for key \"%s\", value \"%s\"", *cacheKey, valueBytes)
+		return err
+	}
+	cacheKeysetName := ServiceCacheKeySetPrefix + serviceName
+	err = storage.SAdd(ctx, cacheKeysetName, *cacheKey)
+	if err != nil {
+		g.Log().Warningf(ctx, "error save cache key \"%s\" to keyset \"%s\"", *cacheKey, cacheKeysetName)
+		return err
+	}
+	return nil
+}
+
+// DeleteCache 根据 cacheKey 删除单个缓存
+// cacheKey 缓存key
+func DeleteCache(ctx context.Context, cacheKey *string) error {
+	if cacheKey == nil {
+		return ErrEmptyCacheKey
+	}
+	if !storage.Initialized() {
+		return ErrCacheNotInitialized
+	}
+	err := storage.Delete(ctx, []string{*cacheKey})
+	if err != nil {
+		g.Log().Warningf(ctx, "error delete cache key \"%s\"", cacheKey)
+		return err
+	}
+	return nil
+}
+
+// ClearCache 清除 service 下所有已经保存的缓存
+func ClearCache(ctx context.Context, serviceName string) error {
+	if !storage.Initialized() {
+		return ErrCacheNotInitialized
+	}
+	cacheKeysetName := ServiceCacheKeySetPrefix + serviceName
+	keys, err := storage.SMembers(ctx, cacheKeysetName)
+	if err != nil {
+		g.Log().Warningf(ctx, "error load cache keyset \"%s\"", cacheKeysetName)
+		return err
+	}
+	err = storage.Delete(ctx, append(keys, cacheKeysetName))
+	if err != nil {
+		g.Log().Warningf(ctx, "error clear cache keys \"%s\"", gstr.Join(keys, ","))
+		return err
+	}
+	return nil
+}
